dd9a92c2c5dcfe65e77683445348f47a873268ae
Yulio Ferdinand <46779184+yulioferd@users.noreply.github.com>
2019-04-06 11:32:35 +0700

    jumlah fibonacci

--- JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -17,18 +17,23 @@ public class JavaApplication1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       int n, a = 0, b = 1, c = 1;
+       int n=0, a = 0, b = 0, c = 1;
         Scanner s = new Scanner(System.in);
         System.out.print("Enter value of n:");
-        n = s.nextInt();
-        System.out.print("Fibonacci Series:");
-        for(int i = 1; i <= n; i++)
+        int sum = 0; 
+        int num = s.nextInt();
+        System.out.print("Fibonacci:");
+        for(int i = 1; i <= num; i++)
         {
+          
+            sum+=c;
             a = b;
             b = c;
             c = a + b;
-            System.out.print(a+" ");
+            
+            
         }
+        System.out.print(sum);
     }
     }
    
